dee46df13f94c4916ae1c575080baf8a68627409
AhmedShoib <90089186+AhmedShoib@users.noreply.github.com>
2026-05-08 06:25:08 +0300

    Update 2-1.py

--- 2-1.py
+++ 2-1.py
@@ -1,5 +1,9 @@
-numbers = list(range(1, 21))#القائمه
-print("Numbers divisible by 3:")#الطباعة
-for num in numbers:#العداد
-    if num % 3 == 0:
-        print(num)
+temp = float(input("Enter the current temperature in Celsius: "))#أدخال المستخدام
+if temp >= 30:
+    print("It's a hot day. Stay hydrated!")#أكبر من 30
+elif 20 <= temp <= 29:
+    print("It's a warm day. Enjoy the weather!")#من 20الي 29
+elif 10 <= temp <= 19:
+    print("It's a cool day. Wear a jacket!")#من10الي19
+else:
+    print("It's a cold day. Stay warm!")#اقل من 10
